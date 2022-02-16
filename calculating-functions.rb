e769057a5398e8ab31f0a7ae8a3e6d93ddb35813
Jade Cormier <cormier.jade@gmail.com>
2022-02-16 17:21:42 +0100

    calculating function solution

--- calculating-functions.rb
+++ calculating-functions.rb
@@ -1,38 +1,75 @@
-def zero
-  0
+def zero(m = nil)
+  return 0 unless m
+  op, num = m
+  0.to_f.send(op.to_sym, num.to_f)
 end
-def one
-  1
+
+def one(m = nil)
+  return 1 unless m
+  op, num = m
+  1.to_f.send(op.to_sym, num.to_f)
 end
-def two
-  2
+
+def two(m = nil)
+  return 2 unless m
+  op, num = m
+  2.to_f.send(op.to_sym, num.to_f)
 end
-def three
-  3
+
+def three(m = nil)
+  return 3 unless m
+  op, num = m
+  3.to_f.send(op.to_sym, num.to_f)
 end
-def four
-  4
+
+def four(m = nil)
+  return 4 unless m
+  op, num = m
+  4.to_f.send(op.to_sym, num.to_f)
 end
-def five
-  5
+
+def five(m = nil)
+  return 5 unless m
+  op, num = m
+  5.to_f.send(op.to_sym, num.to_f)
 end
-def six
-  6
+
+def six(m = nil)
+  return 6 unless m
+  op, num = m
+  6.to_f.send(op.to_sym, num.to_f)
 end
-def seven
-  7
+
+def seven(m = nil)
+  return 7 unless m
+  op, num = m
+  7.to_f.send(op.to_sym, num.to_f)
 end
-def eight
-  8
+
+def eight(m = nil)
+  return 8 unless m
+  op, num = m
+  8.to_f.send(op.to_sym, num.to_f)
 end
-def nine
-  9
+
+def nine(m = nil)
+  return 9 unless m
+  op, num = m
+  9.to_f.send(op.to_sym, num.to_f)
 end
-def plus
+
+def divided_by(m)
+  return '/', m
 end
-def minus
+
+def plus(m)
+  return '+', m
 end
-def times
+
+def minus(m)
+  return '-', m
 end
-def divided_by
+
+def times(m)
+  return '*', m
 end
